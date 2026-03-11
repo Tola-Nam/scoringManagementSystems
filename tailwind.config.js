aea5ca508bd830372363405f1c32317e8831f74d
Tola Nam <namtola4444@gmail.com>
2026-03-11 22:10:15 +0700

    before angular update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,45 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   content: [
+//     "./src/**/*.{html,ts}",
+//   ],
+//   theme: {
+//     extend: {
+//       colors: {
+//         primary: "#0A3981",
+//         warning: "#AB4459",
+//         secondary: "#F000B8",
+//         accent: "#41C9E2",
+//         neutral: "#3D4451",
+//         danger: "#F72C5B",
+//         gold: "#FFB200",
+//         textgreen600: "#16a34a",
+//         textblue500: "#3b82f6",
+//       },
+//     },
+//     screens: {
+//       sm: "640px",
+//       md: "600px",
+//       lg: "1024px",
+//       xl: "1280px",
+//       "2xl": "1536px",
+//     },
+//   },
+//   daisyui: {
+//     themes: ["light", "dark", "cupcake"],
+//   },
+//   plugins: [
+//     require("daisyui")
+//   ],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {
-      colors: {
-        primary: "#0A3981",
-        warning: "#AB4459",
-        secondary: "#F000B8",
-        accent: "#41C9E2",
-        neutral: "#3D4451",
-        danger: "#F72C5B",
-        gold: "#FFB200",
-        textgreen600: "#16a34a",
-        textblue500: "#3b82f6",
-      },
-    },
-    screens: {
-      sm: "640px",
-      md: "600px",
-      lg: "1024px",
-      xl: "1280px",
-      "2xl": "1536px",
-    },
+    extend: {},
   },
-  daisyui: {
-    themes: ["light", "dark", "cupcake"],
-  },
-  plugins: [
-    require("daisyui")
-  ],
+  plugins: [require("daisyui")],
 }
